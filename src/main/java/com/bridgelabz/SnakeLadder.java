727b391e7f8492194b726db115b3d1e6d7b8da40
tushal012 <tushalkashyap11@gmail.com>
2021-05-27 20:28:48 +0530

    Added: Player condition

--- src/main/java/com/bridgelabz/SnakeLadder.java
+++ src/main/java/com/bridgelabz/SnakeLadder.java
@@ -1,17 +1,37 @@
 package com.bridgelabz;
 
 public class SnakeLadder {
+    public static final int NO_PLAY = 0 ;
+    public static final int LADDER = 1 ;
+    public static final int SNAKE = 2 ;
 
 
     public static void main(String[] args) {
         System.out.println("welcome to snake and ladder program");
 
 
-        int startPostion=0;
+        int position=0;
+        int newPosition=0;
 
         int dieRolls = (int) (Math.random()*6)+1;
+        int checkOption = (int) (Math.random()*3);
 
-        System.out.println(dieRolls);
+        switch (checkOption) {
+            case NO_PLAY :
+                newPosition = 0;
+                break;
+            case LADDER :
+                newPosition = dieRolls ;
+                break;
+            case SNAKE :
+                newPosition = -dieRolls ;
+                break;
+        }
+        position=position+newPosition;
+        if (position<0) {
+            position = 0;
+        }
+        System.out.println("your are now at "+position+"th"+" position");
 
     }
 }
