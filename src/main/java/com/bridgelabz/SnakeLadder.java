039904fcc0c99865fbd5ec8800314956b45022e1
tushal012 <tushalkashyap11@gmail.com>
2021-05-27 16:04:29 +0530

    ADDED: start position

--- src/main/java/com/bridgelabz/SnakeLadder.java
+++ src/main/java/com/bridgelabz/SnakeLadder.java
@@ -5,8 +5,13 @@ public class SnakeLadder {
 
     public static void main(String[] args) {
         System.out.println("welcome to snake and ladder program");
-        
+
+
+        int startPostion=0;
+
     }
 }
+
+
 
 
