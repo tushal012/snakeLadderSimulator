c6306d75349140e04bee1d0ce354124ad4b76bf4
tushal012 <tushalkashyap11@gmail.com>
2021-05-27 15:13:42 +0530

    Welcome to snake and ladder simulator

---
+++ src/main/java/com/bridgelabz/SnakeLadder.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class SnakeLadder {
+
+
+    public static void main(String[] args) {
+        System.out.println("welcome to snake and ladder program");
+        
+    }
+}
+
+
---
+++ src/main/java/com/bridgelabz/SnakeLadderTest.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class SnakeLadderTest {
+
+
+    public static void main(String[] args) {
+        System.out.println("welcome to snake and ladder program");
+        SnakeLadder s = new SnakeLadder();
+        s.startGame();
+    }
+}
